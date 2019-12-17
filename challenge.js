72b47fb5962ee99be457f5eb7c5b05fb8b4a4848
aminamos <26092352+aminamos@users.noreply.github.com>
2019-12-17 04:33:02 -0800

    draft

--- challenge.js
+++ challenge.js
@@ -1,10 +1,45 @@
-window.addEventListener("DOMContentLoaded", function(event) {
+window.addEventListener('DOMContentLoaded', function(event) {
   let counter = 0;
+  let seconds = setInterval(incrementSeconds, 1000);
+  let clicked = false;
+  // let num = document.querySelector("#counter").innerText;
 
-  setInterval(function() {
+  function incrementSeconds() {
     counter += 1;
-    document.querySelector("#counter").innerText = counter.toString();
-  }, 1000);
+    document.querySelector('#counter').innerText = counter.toString();
+  }
 
-  document.querySelector('#minus').addEventListener
+  document.querySelector('#minus').addEventListener('click', function(event) {
+    counter -= 1;
+    document.querySelector('#counter').innerText = counter.toString();
+  });
+
+  document.querySelector('#plus').addEventListener('click', function(event) {
+    counter += 1;
+    document.querySelector('#counter').innerText = counter.toString();
+  });
+
+  document.querySelector('#heart').addEventListener('click', function(event) {
+    console.log('click');
+  });
+
+  document.querySelector('#pause').addEventListener('click', function(event) {
+    clearInterval(seconds);
+    document.querySelector('#pause').innerText = 'Resume';
+    document.querySelector('#pause').id = 'resume';
+    clicked = true;
+  });
+
+  if (clicked) {
+    document
+      .querySelector('#resume')
+      .addEventListener('click', function(event) {
+        // incrementSeconds();
+        document.querySelector('#resume').innerText = 'pause';
+        document.querySelector('#resume').id = 'pause';
+      });
+    clicked = false;
+  }
+
+  incrementSeconds();
 });
